baadb08822844b523584fcbf078c58a41bf1d3d9
saadmomand6 <saadmohammad608@gmail.com>
2024-05-04 10:51:27 +0500

    day1 of course

---
+++ d1_print_input_variables.py
@@ -0,0 +1,22 @@
+# print("Asalamualaikum Saad, May Allah give you success and happyness and congratulation on starting this course , i appreciate your work, thanks again")
+# # ////////////////////PRINT , DECLARING VARIABLES, INPUT////////////////// 
+# print("your name contain"  + " " + str(len(input("what your name?"))))
+# # this will assign the typed thing into this variable
+# name = input("how much money you make ?")
+# print(f"your name is {name}")
+
+# ////////////////////EXERCISE 1.4 = VARIBLES//////////////////
+
+# a = 5
+# b = 100
+# # swape both values
+# c = a 
+# a = b
+# b= c
+# print(a , b)
+
+# /////////////BAND NAME GENERATOR EXCERCISE////////
+print("Welcome to Band name generator")
+city_name = input("what's the city you grew up in")
+pet_name = input("what's your's pet name")
+print(f"so your band name will be {city_name} {pet_name}")
